3a1d8771fbdb1a340cc77968541e494f83bab6d5
PoornachanderReddy <poornachander295@gmail.com>
2025-07-24 09:59:12 +0530

    Update endPoints.js

--- src/server/endPoints.js
+++ src/server/endPoints.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:8888'
+export const baseUrl = 'https://anon-api-r4wr.onrender.com'
 const  paths={
     baseUrl,
     createRoom : baseUrl+'/api/createRoom',
