ab53ff452c93594b67bb28fbf4fa0839b4d07f7e
PoornachanderReddy <poornachander295@gmail.com>
2025-08-04 02:53:25 +0530

    final 1

--- src/pages/genAi/GenAiChatBox.jsx
+++ src/pages/genAi/GenAiChatBox.jsx
@@ -32,7 +32,7 @@ const GenAiChatBox = ({ messages,loading }) => {
       ))}
       {loading && (
         <div className="flex justify-center">
-          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+          <div className="animate-spin rounded-full h-8 w-8 border-b-2 "></div>
         </div>
       )}
     </div>
