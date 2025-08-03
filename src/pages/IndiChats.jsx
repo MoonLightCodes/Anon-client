94ebac10e0a8bb0677b5e130c665046a97daf08a
PoornachanderReddy <poornachander295@gmail.com>
2025-08-04 03:02:23 +0530

    final 3

--- src/pages/IndiChats.jsx
+++ src/pages/IndiChats.jsx
@@ -114,7 +114,7 @@ const IndiChats = () => {
           }`}
         >
           <p className="text-white break-words">{message.text}</p>
-          <p className="text-slate-900 text-xs mt-1">
+          <p className="text-gray-400 text-xs mt-1">
             {isOwn ? "You" : message?.sender?.username || "Other"} •{" "}
             {moment(message.createdAt).format("h:mm A")}
           </p>
